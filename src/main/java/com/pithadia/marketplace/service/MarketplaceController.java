bc95f5b7cbfd679eea82c5859d77dd26b22c7b20
Rakshit Pithadia <rpithadia25@gmail.com>
2018-01-30 18:00:19 -0800

    Removed unused imports

--- src/main/java/com/pithadia/marketplace/service/MarketplaceController.java
+++ src/main/java/com/pithadia/marketplace/service/MarketplaceController.java
@@ -7,7 +7,6 @@ import com.pithadia.marketplace.exception.UserUnauthorizedException;
 import com.pithadia.marketplace.request.BidAddRequest;
 import com.pithadia.marketplace.request.BidDeleteRequest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
